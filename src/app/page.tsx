404d07b0d8357657f39c62057462e23a004e7248
omkar <omkar@saiashirwad.com>
2026-08-10 02:08:23 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,6 @@ import KeyMetrics from '@/components/landing/KeyMetrics';
 import Contact from '@/components/landing/Contact';
 
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-
-
 
 export default function Home() {
   return (
